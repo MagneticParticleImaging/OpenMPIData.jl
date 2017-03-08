44524d1a8aaa7392c88065ce3363bc08585f02ab
autodocs <autodocs>
2017-03-08 22:00:43 +0000

    build based on fd25ab1

---
+++ latest/search_index.js
@@ -0,0 +1,59 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Open-MPI-Data-Initiative-1",
+    "page": "Home",
+    "title": "Open MPI Data Initiative",
+    "category": "section",
+    "text": "Initiative for open Magnetic Particle Imaging Data *"
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Home",
+    "title": "Introduction",
+    "category": "section",
+    "text": "OpenMPIData is ..."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Install OpenMPIData within Julia usingPkg.clone(\"https://github.com/tknopp/OpenMPIData.jl.git\")"
+},
+
+{
+    "location": "index.html#Usage-1",
+    "page": "Home",
+    "title": "Usage",
+    "category": "section",
+    "text": "See Getting Started for an introduction to using the package"
+},
+
+{
+    "location": "gettingStarted.html#",
+    "page": "Getting Started",
+    "title": "Getting Started",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "gettingStarted.html#Getting-Started-1",
+    "page": "Getting Started",
+    "title": "Getting Started",
+    "category": "section",
+    "text": "TODO"
+},
+
+]}
